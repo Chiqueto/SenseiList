3fce73180b143f3687c1cad36d73bab10c49c887
chiqueto <lfchiqueto@gmail.com>
2025-03-25 16:49:05 -0300

    adjust status bar

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import Routes from "./src/routes";
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor="black" />
       <Routes />
     </NavigationContainer>
   );
