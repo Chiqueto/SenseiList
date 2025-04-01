dd59b775b32bc0cd73da3a898a538371f778b5a4
Chiqueto <lfchiqueto@gmail.com>
2025-04-01 16:12:59 -0300

    fix: alter fonts load

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import Routes from "./src/routes";
+import { useFonts } from "expo-font";
+import { ActivityIndicator } from "react-native";
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Fredoka-Regular": require("./assets/fonts/Fredoka/static/Fredoka-Regular.ttf"),
+    "Fredoka-Bold": require("./assets/fonts/Fredoka/static/Fredoka-Bold.ttf"),
+    "Fredoka-Medium": require("./assets/fonts/Fredoka/static/Fredoka-Medium.ttf"),
+    "Fredoka-SemiBold": require("./assets/fonts/Fredoka/static/Fredoka-SemiBold.ttf"),
+    "Poppins-Bold": require("./assets/fonts/Poppins/Poppins-Bold.ttf"),
+    "Poppins-Medium": require("./assets/fonts/Poppins/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("./assets/fonts/Poppins/Poppins-Regular.ttf"),
+    "Poppins-Light": require("./assets/fonts/Poppins/Poppins-Light.ttf"),
+    "Poppins-SemiBold": require("./assets/fonts/Poppins/Poppins-SemiBold.ttf"),
+    "Poppins-LightItalic": require("./assets/fonts/Poppins/Poppins-LightItalic.ttf"),
+  });
+  if (!fontsLoaded) {
+    return <ActivityIndicator color="red" size={"large"} />;
+  }
   return (
     <NavigationContainer>
       <StatusBar backgroundColor="rgba(0, 0, 0, 0.2)" />
--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,12 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./pages/Home/index";
 import Login from "./pages/Login/index";
-import { useFonts } from "expo-font";
 import Register from "./pages/Register/index";
 import Watched from "./pages/Watched";
 import AnimePage from "./pages/Anime/index";
 
 const Stack = createStackNavigator();
 const Routes = () => {
-  const [fontsLoaded] = useFonts({
-    "Fredoka-Regular": require("../assets/fonts/Fredoka/static/Fredoka-Regular.ttf"),
-    "Fredoka-Bold": require("../assets/fonts/Fredoka/static/Fredoka-Bold.ttf"),
-    "Fredoka-Medium": require("../assets/fonts/Fredoka/static/Fredoka-Medium.ttf"),
-    "Fredoka-SemiBold": require("../assets/fonts/Fredoka/static/Fredoka-SemiBold.ttf"),
-    "Poppins-Bold": require("../assets/fonts/Poppins/Poppins-Bold.ttf"),
-    "Poppins-Medium": require("../assets/fonts/Poppins/Poppins-Medium.ttf"),
-    "Poppins-Regular": require("../assets/fonts/Poppins/Poppins-Regular.ttf"),
-    "Poppins-Light": require("../assets/fonts/Poppins/Poppins-Light.ttf"),
-    "Poppins-SemiBold": require("../assets/fonts/Poppins/Poppins-SemiBold.ttf"),
-    "Poppins-LightItalic": require("../assets/fonts/Poppins/Poppins-LightItalic.ttf"),
-  });
   return (
     <Stack.Navigator>
       <Stack.Screen
